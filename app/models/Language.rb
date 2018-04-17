2b5198f9a3572c65bf12670166d3b60aea0b8489
github username <github email address>
2018-04-17 17:18:34 -0400

    corrected monster methods

--- app/models/Language.rb
+++ app/models/Language.rb
@@ -1,5 +1,17 @@
 class Language < ActiveRecord::Base
   belongs_to :monster
+
+  def which_monsters_speak
+    Monster.all.map do |mon|
+      mon[:languages] == self
+    end
+  end #HUGE POTENTIAL PROBLEM because languages are stored as one string that has to be split
+
+
+
+
+
+
 end
 
 
--- app/models/Monster.rb
+++ app/models/Monster.rb
@@ -10,46 +10,46 @@ class Monster < ActiveRecord::Base
 
 # NOT SURE WHAT monster_key (the hash) is actually called
   def self.of_this_size(size)
-    Monster.all.map do |mon|
+    Monster.all.select do |mon|
       mon[:size] == size
     end #or use SELECT?
   end
 
   def self.easier_than(difficulty)
-     Monster.all.map do |mon|
+     Monster.all.select do |mon|
        if mon.challenge_rating < difficulty
-         mon
+         mon.name
        end
      end
    end
 
   def self.harder_than(difficulty)
-     Monster.all.map do |mon|
+     Monster.all.select do |mon|
        if mon.challenge_rating > difficulty
-         mon
+         mon.name
        end  #for this and above OR mon[:challenge_rating] instead of .challenge_rating
      end
    end
 
    def self.between_easy_and_hard(low_difficulty, high_difficulty)
-     Monster.all.map do |mon|
-       if mon[:challenge_rating] > low_difficulty && mon[:challenge_rating] < high_difficulty
+     Monster.all.select do |mon|
+       if mon.challenge_rating > low_difficulty && mon.challenge_rating < high_difficulty
          mon
        end
      end
    end
 
   def self.who_speak(language)
-    Monster.all.map do |mon|
-      if mon[:languages] == language
+    Monster.all.select do |mon|
+      if mon.languages == language
         mon
       end
     end
   end
 
   def is_successfully_attacked(user_attack_damage) #<< NO CLUE IF WE'LL EVER HAVE THAT ARGUMENT TO GIVE
-    self[:hit_points] -= user_attack_damage
-    self[:hit_points]
+    self.hit_points -= user_attack_damage
+    self.hit_points
   end
 
   # def abilities
